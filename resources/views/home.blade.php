80a91527bf55e271fbadbb8c5fd054d8912dc368
Syaqir Zahir <hikigayasyaqir@gmail.com>
2023-01-05 21:17:01 +0800

    font itaallic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,7 +33,7 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item font-weight-bolder"><a class="nav-link" href="/main">About</a></li>
+                        <li class="nav-item font-italic"><a class="nav-link" href="/main">About</a></li>
                         <li class="nav-item"><a class="nav-link" href="/home">Home</a></li>
                         <li class="nav-item"><a class="nav-link" href="/googleMaps">Maps</a></li>
 
