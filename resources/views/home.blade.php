1ec6fbba65ed7f88952618cdb7b8498a4a345de3
Syaqir Zahir <hikigayasyaqir@gmail.com>
2023-01-05 21:05:01 +0800

    tukaq font

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
 
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="/main">ReportTruzz</a>
+                <a class="navbar-brand font-weight-bolder" href="/main">ReportTruzz</a>
                 <a class="navbar-brand">{{ Auth::user()->name }}</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
