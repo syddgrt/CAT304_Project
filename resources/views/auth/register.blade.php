e520bd11e76f1fd8c2dd17a3deff846f9d02eed9
Syaqir Zahir <hikigayasyaqir@gmail.com>
2023-01-29 14:59:20 +0800

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -107,7 +107,7 @@
 
             <div class="col-12">
                 
-                <button class="btn btn-primary w-100" type="submit">Create Account</button>
+                <button class="btn btn-primary w-100 active" type="submit">Create Account</button>
             </div>
 
             <div class="col-12">
